bb3b528464cca4bd2e23aee6af10c3075027beae
Vexta <vektornado@gmail.com>
2026-03-07 17:50:43 +0800

    Update script.sh

--- files/script.sh
+++ files/script.sh
@@ -3,7 +3,7 @@
 rsync -a --delete --checksum $HOME/ /tmp/backup
 
 if [ $? -eq 0 ]; then
-    logger -t home_backup "rsync done"
+    /usr/bin/logger -t home_backup "rsync done"
 else
-    logger -t home_backup "rsync failed"
+    /usr/bin/logger -t home_backup "rsync failed"
 fi
