0a13f0e3ecb67db0b9ca508dccb1b3e89790de6e
Vexta <vektornado@gmail.com>
2026-03-07 17:51:10 +0800

    Update script.sh

--- files/script.sh
+++ files/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rsync -a --delete --checksum $HOME/ /tmp/backup
+rsync -a --delete --checksum --exclude='.*' $HOME/ /tmp/backup
 
 if [ $? -eq 0 ]; then
     /usr/bin/logger -t home_backup "rsync done"
